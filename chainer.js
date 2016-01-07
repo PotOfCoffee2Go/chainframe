7b1fdb07567539529200212434282daf9562ad40
PotOfCoffee2Go <kim@lrunit.net>
2016-01-07 04:43:27 -0500

    Commenting ,  more refactoring, and code clean up

--- chainer.js
+++ chainer.js
@@ -7,26 +7,40 @@ const util = require('util');
 const fs = require('fs');
 const EventEmitter = require('events');
 
-function Method(fn, aArgs, isasync, callbackArgIdx) {
-    this.fn = fn || this.placeholder;
+/*************************************
+ * Returns a Method object which can be chained with other Method objects
+ *
+ * @param fn                function that this Method calls
+ * @param aArgs             arguments passed to 'fn'
+ * @param isAsync           is 'fn' an asynchronous function?
+ * @param callbackArgIdx    if 'fn' has a callback in 'aArgs' where is it?
+ * @constructor
+ */
+function Method(fn, aArgs, isAsync, callbackArgIdx) {
+    this.fn = fn || this.placeholderFn;
     this.aArgs = aArgs || null;
-    this.isasync = isasync || false;
+    this.isAsync = isAsync || false;
     this.callbackArgIdx = callbackArgIdx || -1;
     this.prevMethod = null;
 }
 
-// Placeholder Method returns the arguments passed to it (as if it were not even in the Method chain)
-Method.prototype.setPlaceholder = function () {
-    this.fn = this.placeholder;
+// Note: a Placeholder Method returns the arguments passed to it
+//  (as if it were not even in the Method chain)
+
+// Reset a Method to be a placeholder
+Method.prototype.setAsPlaceholder = function () {
+    this.fn = this.placeholderFn;
     this.aArgs = null;
-    this.isasync = false;
+    this.isAsync = false;
     this.callbackArgIdx = -1;
     this.prevMethod = null;
 };
-// Placeholder function returns the arguments passed to it
-Method.prototype.placeholder = function () {
+// The Placeholder function returns the arguments passed to it
+Method.prototype.placeholderFn = function () {
     return [].slice.call(arguments);
 };
+
+// Run the Methods that have been placed on the Method stack
 Method.prototype.run = function (target, stackControl) {
     // drill down to the placeholder Method at bottom of the Method stack
     if (this.prevMethod) {
@@ -39,31 +53,36 @@ Method.prototype.run = function (target, stackControl) {
         //   if none - use arguments that were returned by the previous method
         if (!this.aArgs) {
             if (Object.prototype.toString.call(prevResult) === '[object Array]') {
-                this.aArgs = prevResult;
+                this.aArgs = prevResult; // previous Method returned an Array - so we are good
             }
             else {
-                this.aArgs = new Array(prevResult);
+                this.aArgs = new Array(prevResult); // otherwize make an Array
             }
         }
     }
-    // if async will stop execution of the stack
-    if (this.isasync) {
+    // if async Method need to stop execution of the stack - resume when async done
+    if (this.isAsync) {
         // remember the async function to call and it's arguments
         //  and where the callback is located in the arguments (if any)
         stackControl.fn = this.fn;
         stackControl.aArgs = this.aArgs;
         stackControl.callbackArgIdx = this.callbackArgIdx;
         // make this Method a placeholder function (for re-entry when async function is done)
-        this.setPlaceholder();
-        // signal to exit by returning with the stackControl object
+        this.setAsPlaceholder();
+        // signal to exit the stack by returning with the stackControl object
         return stackControl;
     }
-    // run the function
+    // is a synchronous Method so just run the function
     var result = this.fn.apply(target, this.aArgs);
-    this.setPlaceholder();
+    this.setAsPlaceholder();
     return result;
 };
 
+/*************************************
+ * Returns MethodChainer object that maintains the Method List and Method Stack
+ * @param target    Methods will be bound (ie: 'this') to 'target' object
+ * @constructor
+ */
 function MethodChainer(target) {
     // Methods will be bound ('this') to target object
     this.target = target;
@@ -88,25 +107,30 @@ function MethodChainer(target) {
 // Inherit functions from 'EventEmitter' prototype
 util.inherits(MethodChainer, EventEmitter);
 
+// Links 'method' to the current Method on the top of the methodStack
+//  and makes 'method' the topmost Method on methodStack
 MethodChainer.prototype.push = function (method) {
     method.prevMethod = this.methodStack;
     this.methodStack = method;
 };
+
+// Sequentially run Methods from the bottom of methodStack to the top
 MethodChainer.prototype.run = function () {
     // got arguments so place them in the placeholder Method at the bottom of the stack
     if (arguments.length) {
         var bottomMethod = this.methodStack;
-        // work our way to the placeholder Method at the bottom of the stack
+        // work our way down to the placeholder Method at the bottom of the stack
         while (bottomMethod.prevMethod) {
             bottomMethod = bottomMethod.prevMethod;
         }
         // set it's arguments
         bottomMethod.aArgs = [].slice.call(arguments);
     }
-    // run the Method stack - info will be in stackControl
+    // Info required to run async functions will be in inserted into stackControl object
     var stackControl = {};
+    // run the Method stack
     this.methodStack.run(this.target, stackControl);
-    // if stackControl contains a function - run it - (will be an asynchronous function)
+    // if stackControl contains an asynchronous function - run it
     if (stackControl.fn) {
         if (stackControl.callbackArgIdx === -1) {
             stackControl.aArgs.push(this.run.bind(this))
@@ -119,19 +143,32 @@ MethodChainer.prototype.run = function () {
 };
 
 
-function InheritChainCalls() {
-    this.chainer = new MethodChainer(this);
+/*************************************
+ * Helper object that has commonly used MethodChainer functions in prototype
+ *  inherit into your subtypes for easy access to MethodChainer
+ * @constructor
+ */
+function InheritChainerCalls() {
+    this.methodChainer = new MethodChainer(this);
 }
-InheritChainCalls.prototype.chainPush = function (method) {
-    this.chainer.push(method);
+// Add a Method to the list of chainable Methods
+InheritChainerCalls.prototype.chainAdd = function (method) {
+    this.methodChainer.add(method);
 };
 
-InheritChainCalls.prototype.chainRun = function (method) {
-    this.chainer.run(method);
+// Push a method onto the methodStack
+InheritChainerCalls.prototype.chainPush = function (method) {
+    this.methodChainer.push(method);
 };
 
-InheritChainCalls.prototype.chainEmit = function (eventName, method) {
-    this.chainer.emit(eventName, method);
+// Run Methods on the methodStack
+InheritChainerCalls.prototype.chainRun = function (method) {
+    this.methodChainer.run(method);
+};
+
+// Emit an add, push, or run event to MethodChainer - does same as functions above
+InheritChainerCalls.prototype.chainEmit = function (eventName, method) {
+    this.methodChainer.emit(eventName, method);
 };
 
 
@@ -140,8 +177,8 @@ InheritChainCalls.prototype.chainEmit = function (eventName, method) {
  * @constructor
  */
 function Mine() {
-    // init inherited 'MixinChain'
-    InheritChainCalls.call(this);
+    // init inherited 'InheritChainerCalls'
+    InheritChainerCalls.call(this);
 //    this.chainer.push(new Method(this.hi1, ['starter'])); // overriding args
     this.chainPush(new Method(this.hi1));
     this.chainEmit('push', new Method(this.hi2, null, true));
@@ -153,8 +190,8 @@ function Mine() {
     this.chainRun();
 }
 
-// Inherit functions from InheritChainCalls prototype
-util.inherits(Mine, InheritChainCalls);
+// Inherit functions from InheritChainerCalls prototype
+util.inherits(Mine, InheritChainerCalls);
 
 Mine.prototype.hi1 = function (lastguy) {
     console.log('hi1');
@@ -182,11 +219,13 @@ Mine.prototype.hi4 = function (lastguy) {
 };
 
 Mine.prototype.readMyFile = function (inFilePath, cb) {
+    console.log('read the file: %s', inFilePath);
     fs.readFile(inFilePath, cb);
 };
 Mine.prototype.processMyFile = function (err, data) {
     if (err) throw err;
     var MyData = JSON.parse(data);
+    console.log('what was read :');
     console.log(util.inspect(MyData));
 };
 
