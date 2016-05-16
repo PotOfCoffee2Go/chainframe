8d87b6a8b307be7fc5c241ee5220132fc87b678e
PotOfCoffee2Go <kim@lrunit.net>
2016-05-16 08:20:43 -0400

    Review of data-from-api example

--- examples/data-from-api/index.js
+++ examples/data-from-api/index.js
@@ -1,16 +1,18 @@
 /**
  * Created by PotOfCoffee2Go on 1/18/2016.
  *
- * kudos to jsonplaceholder for making available a REST API for testing
- *   http://jsonplaceholder.typicode.com/
- *
- * Quick and dirty task:
- *   My boss wants me to get the To Do List from jsonplaceholder site and output to a csv file.
- *
- * ChainFrame allows me to do it without ending up in Callback Hell/Pyramid of Doom;
- *  using npm modules like 'step' or 'async'; nor using events, promises... etc.
- *
  */
+
+/// kudos to jsonplaceholder for making available a REST API for testing
+///   http://jsonplaceholder.typicode.com/
+///
+/// Quick and dirty task:
+///   Get the To Do List from jsonplaceholder site and output to a csv file.
+///
+/// ChainFrame allows me to do it without ending up in Callback Hell/Pyramid of Doom;
+///  using npm modules like 'step' or 'async'; nor using events, promises... etc.
+///
+
 "use strict";
 
 var util = require('util');
@@ -19,14 +21,14 @@ var fs = require('fs');
 
 var ChainFrame = require('../../ChainFrame');
 
-// A canned function of mine - use it a lot
+/// A canned log function - use it a lot
 function log(data) {
     console.log(util.inspect(data));
     return (data);
 }
 
-// Request 'to do list' from jsonplaceholder
-function httpRequestForTodoList(callback) {
+/// Request 'to do list' from jsonplaceholder
+function httpRequestTodoList(callback) {
     var options = {
         host: 'jsonplaceholder.typicode.com',
         path: '/todos'
@@ -35,8 +37,8 @@ function httpRequestForTodoList(callback) {
     http.request(options, callback).end();
 }
 
-// Gather up the data from a nodejs 'http' request
-//  note: nodejs 'http' uses events - but nothing to do with ChainFrame
+/// Gather up the data from a nodejs 'http' request
+/// > note: nodejs 'http' uses events - but nothing to do with ChainFrame
 function gatherHttpResponse(response, callback) {
     var str = '';
 
@@ -51,12 +53,12 @@ function gatherHttpResponse(response, callback) {
     });
 }
 
-// Convert a string representation of JSON data to a JavaScript object
+/// Convert a string representation of JSON data to a JavaScript object
 function convertResponseToObject(strData) {
     return JSON.parse(strData);
 }
 
-// Transform 'to do list' JSON data into a CSV format
+/// Transform 'to do list' JSON data into a CSV format
 function transformTodoListToCsv(todoList) {
     var csv = '"userId","id","title","completed"\n';
     todoList.forEach(function (todo) {
@@ -70,42 +72,43 @@ function transformTodoListToCsv(todoList) {
     return csv;
 }
 
-// Write data to a file
+/// Write data to a file
 function saveToFile(data, callback) {
     fs.writeFile('todo.csv', data, callback);
 }
 
-// Check for error
+/// Check for errors
 function checkForErrors(err) {
     if (err) throw err;
 }
 
+/// Report all done
 function allDone() {
     console.log("todo.csv saved in 'data-from-api' directory. Open it and check it out!");
 }
 
-// We're gonna chain together the above functions (asynchronous ones and all) - hehehe ;)
+/// We're gonna chain together the above functions (asynchronous ones and all) - hehehe ;)
 var todoList = new ChainFrame();
 
-// Add above functions to the 'todoList' ChainFrame instance
-//  Could have used the shortcut 'todoList.addToInstanceAll();' instead
-//   but thought adding functions one at a time more informative
+/// Add above functions to the 'todoList' ChainFrame instance
+///  Could have used the shortcut 'todoList.addToInstanceAll();' instead
+///   but thought adding functions one at a time more informative
 todoList
-        .addToInstance(httpRequestForTodoList)
+        .addToInstance(httpRequestTodoList)
         .addToInstance(gatherHttpResponse)
         .addToInstance(convertResponseToObject)
         .addToInstance(transformTodoListToCsv)
         .addToInstance(saveToFile)
         .addToInstance(checkForErrors)
         .addToInstance(allDone)
-        .addToInstance(log)
+        .addToInstance(log);
 
-        // Build the method chain which produces the CSV file of the to-do list
-        .httpRequestForTodoList()
+/// Build the method chain which produces the CSV file of the to-do list
+todoList
+        .httpRequestTodoList()
         .gatherHttpResponse()
         .convertResponseToObject()
         .transformTodoListToCsv()
-        //.log()
         .saveToFile()
         .checkForErrors()
         .allDone()
@@ -113,5 +116,5 @@ todoList
         // Run the chain
         .runChain();
 
-// Did you notice that 'return this;' required for method chaining was not used once!
-// Such is the magic of ChainFrame!!!
+/// Did you notice that 'return this;' required for method chaining was not used?
+/// Such is the magic of ChainFrame!!!
